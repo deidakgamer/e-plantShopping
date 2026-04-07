6595656e01ae91d1ea6c7de27d5da150d32e0faf
deidakgamer <davichoapestosito4@gmail.com>
2026-04-07 16:02:17 -0500

    Complete cart reducers

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,11 +7,40 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
+  const item = action.payload;
+
+  const existingItem = state.items.find(
+    (i) => i.name === item.name
+  );
+
+  if (existingItem) {
+    existingItem.quantity += 1;
+  } else {
+    state.items.push({ ...item, quantity: 1 });
+  }
+
     
     },
     removeItem: (state, action) => {
-    },
-    updateQuantity: (state, action) => {
+     
+  const name = action.payload;
+
+  state.items = state.items.filter(
+    (item) => item.name !== name
+  );
+},
+    
+   updateQuantity: (state, action) => {
+  const { name, quantity } = action.payload;
+
+  const item = state.items.find(
+    (i) => i.name === name
+  );
+
+  if (item && quantity > 0) {
+    item.quantity = quantity;
+  }
+
 
     
     },
